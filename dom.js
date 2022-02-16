e903d42023679798a0c6d456f007c118a8f54557
Esra Uluturk <feuuluturk@gmail.com>
2022-02-16 22:48:28 +0300

    Add Dom Basic Operations

---
+++ dom.js
@@ -0,0 +1,11 @@
+console.log(document.URL)
+console.log(document.baseURI)
+
+console.log(document.location)
+console.log(document.location.hostname) //IP
+console.log(document.location.pathname) //icinde bulundugum dosya adi
+
+console.log(document.body)
+console.log(document.head)
+
+document.body.style.backgroundColor = "red";//javascript change document style
