7e3905249242eb3a69daaaea1f3b2fea6241f3c6
Esra Ulutürk <73850856+esrauluturk@users.noreply.github.com>
2022-04-05 18:41:24 +0300

    delete folder

--- REACT/Task-1/index.js
+++
@@ -1,9 +0,0 @@
-import getData from './moduleGetData.js'
-
-// 1 - 2
-// getData(1)
-
-// Promise
-getData(1)
-    .then((data)=>console.log(data))
-    .catch((error)=>console.log(error));
--- REACT/components/hello-react/src/components/Header.js
+++
@@ -1,5 +0,0 @@
-function Header(){ // Fonksiyon buyuk harfle baslamalidir.
-    return <div>Merhaba Ben Header Bileşeniyim.</div>;
-    
-}
-export default Header;
